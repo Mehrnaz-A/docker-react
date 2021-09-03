888a558b702cf93a4a2d7bfe4b513e167dbb4249
Mehrnaz-A <79409215+Mehrnaz-A@users.noreply.github.com>
2021-09-03 12:37:07 +0200

    adding some files

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>TEST 2!</p>
+        <p>Hei there!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
